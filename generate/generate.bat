d8583d577232f2357fb8f0a6218c49f3c28342eb
Hanjie Zhang <vainjoker@tuta.io>
2024-10-13 07:07:21 +0800

    &

--- generate/generate.bat
+++ generate/generate.bat
@@ -9,10 +9,22 @@ release-plz init
 
 # Generate Codecov token and open settings page
 $codecov_url = "https://app.codecov.io/github/{{github-username}}/{{github-repository}}/settings/"
+Write-Host "Press Enter when you're ready to set up Codecov..."
+Read-Host
 Start-Process $codecov_url
+
+Write-Host "Please complete the Codecov setup in the opened browser window."
+Write-Host "Press Enter when you've finished setting up Codecov..."
+Read-Host
 
 # Link Mergify
 $mergify_url = "https://dashboard.mergify.com/integrations/github?repository={{github-username}}/{{github-repository}}&login={{github-username}}"
+Write-Host "Press Enter when you're ready to set up Mergify..."
+Read-Host
 Start-Process $mergify_url
 
-Write-Host "Please complete the setup for Codecov and Mergify in the opened browser windows."
+Write-Host "Please complete the Mergify setup in the opened browser window."
+Write-Host "Press Enter when you've finished setting up Mergify..."
+Read-Host
+
+Write-Host "Setup process completed."
